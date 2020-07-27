25cdcc6d0f4885514c2ca000439f76fed43aef13
allbb3886 <brandonduong@outlook.com>
2020-07-27 16:42:13 -0400

    Very brief notes from chapter 4.

---
+++ Chapter4.R
@@ -0,0 +1,31 @@
+#Chapter 4
+
+
+
+library('tidyverse')
+1/200 *30
+
+x<- 3*4
+
+object_name <- value
+
+# object_name gets value
+
+this_is_a_long_name <- 2.5
+
+
+## calling functions:
+
+# function_name(arg1=val1, arg2= val2,..)
+
+seq(1,10)
+y <- "hello world"
+
+z <- seq(1,10,length.out=5)
+
+
+ggplot(dota = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+filter(mpg, cyl == 8)
+filter(diamonds, carat > 3)
